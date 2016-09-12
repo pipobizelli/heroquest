eaf3fc36c4a3b195a1b91e7fcc415e9258787399
Renato Alves <renatodex@gmail.com>
2016-09-11 22:54:02 -0300

    Implemented action route

--- server/app.js
+++ server/app.js
@@ -94,18 +94,22 @@ function call_active_action(session_id) {
   }).then(function(snapshot) {
     var heroes = snapshot.child("_setup/heroes").val();
     var rounds = snapshot.child("rounds").val();
-
-    if(rounds == null) {
-      call_create_round(session_id);
-      return build_action_json("waiting_new_round", {});
-    }
-
     var last_round = __.values(rounds)[snapshot.child("rounds").numChildren()-1];
+
+    // Usado nos métodos de validação de action
+    var last_round_id = __.keys(rounds)[snapshot.child("rounds").numChildren()-1];
     var heroes_array = __.keys(heroes);
 
     var actions_array = __.map(__.values(last_round), function(obj) {
       return obj.summoned_id;
     })
+
+    if(rounds == null) {
+      // call_create_round(session_id);
+      return build_action_json("active_turn", {
+        hero_id: __.keys(heroes)[0]
+      });
+    }
 
     var last_hero_id = __.last(actions_array);
 
@@ -117,15 +121,19 @@ function call_active_action(session_id) {
 
     if(total_actions_last_hero < MAX_ACTIONS) {
       json_response = build_action_json("active_turn", {
-        hero_id: last_hero_id
+        hero_id: last_hero_id,
+        round_id: last_round_id
       });
     } else {
       if(__.isEmpty(remaining_turns)) {
-        call_create_round(session_id);
-        json_response = build_action_json("waiting_new_round", {});
+        // call_create_round(session_id);
+        json_response = build_action_json("active_turn", {
+          hero_id: __.keys(heroes)[0]
+        });
       } else {
         json_response = build_action_json("active_turn", {
-          hero_id: __.first(remaining_turns)
+          hero_id: __.first(remaining_turns),
+          round_id: last_round_id
         });
       }
     }
@@ -305,8 +313,45 @@ app.get('/session/:session_id/turn', function(req, res) {
   });
 });
 
-app.get('/session/turn/finish', function(req, res) {
-  res.send('Finish your turn...')
+app.post('/session/:session_id/action/:action_type/:thing_id', function(req, res) {
+  call_active_action(req.params.session_id || "missigno").then(function(json_response) {
+    var roundkey;
+
+    if(__.isEmpty(json_response.action.data)) {
+      res.send(json_response);
+    } else {
+      if(req.params.thing_id == json_response.action.data.hero_id) {
+
+
+        if(json_response.action.data.round_id) {
+          roundkey = json_response.action.data.round_id;
+        } else {
+          var roundref = db.child('sessions').child(req.params.session_id).child("rounds").push();
+          roundkey = roundref.key;
+        }
+        var pushref = db
+          .child('sessions')
+          .child(req.params.session_id)
+          .child('rounds')
+          .child(roundkey).push();
+
+        var action_object = {
+          position: req.params.position || "1,1",
+          summoned_id: json_response.action.data.hero_id,
+          action_type: req.params.action_type
+        };
+
+        pushref.set(action_object);
+        res.send(build_action_json("action_saved", action_object));
+
+      } else {
+        res.send(build_error_json(666, "Não vem hackear aqui não!"));
+      }
+    }
+
+  }).catch(function(error) {
+    console.log(error);
+  })
 });
 
 app.listen(3000, function () {
