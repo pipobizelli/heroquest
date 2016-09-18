1c96a6a8fd2c97dacb16fd1766a6b030d4a84725
Renato Alves <renatodex@gmail.com>
2016-09-18 05:59:15 -0300

    Added full turn cycle. Almost finishing movement poc

--- client/controlable.js
+++ client/controlable.js
@@ -5,19 +5,22 @@ var Controllable = function(game) {
     chosen_sprite = sprite;
   }
 
-  this.control = function() {
+  this.control = function(action_callback) {
     if(game.input.keyboard.downDuration(Phaser.Keyboard.LEFT, 1)) {
-      console.log(state);
       grid.move_left(chosen_sprite);
+      action_callback();
     }
     if(game.input.keyboard.downDuration(Phaser.Keyboard.RIGHT, 1)) {
       grid.move_right(chosen_sprite);
+      action_callback();
     }
     if(game.input.keyboard.downDuration(Phaser.Keyboard.UP, 1)) {
       grid.move_up(chosen_sprite);
+      action_callback();
     }
     if(game.input.keyboard.downDuration(Phaser.Keyboard.DOWN, 1)) {
       grid.move_down(chosen_sprite);
+      action_callback();
     }
   }
 
